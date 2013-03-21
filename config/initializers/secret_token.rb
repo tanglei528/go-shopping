378aa1173aa7b3a7a943d071baf60d13e54bce7e
tang <tanglei528@163.com>
2013-03-21 11:48:56 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GoShopping::Application.config.secret_token = 'f2537acdb33ca7beb22f201c1c36b3dd4713a2026069e40db2b7fdff4f4872c98cb00c4bfb148ad9253e4edd91f3daba806283fdf5ce472a9c8301f50cd54a00'
